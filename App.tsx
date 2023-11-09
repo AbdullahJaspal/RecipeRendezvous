1d79e86bcff0b80b8e19ea55c3c6d2a33503c254
AbdullahJaspal <abdullahafzal4444@gmail.com>
2023-11-09 15:17:22 +0500

    welcome screen

---
+++ App.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import Wellcome from './src/screens/wellcome';
+import {SafeAreaView} from 'react-native';
+
+const App = () => {
+  return <Wellcome />;
+};
+
+export default App;
---
+++ src/theme/theme.js
@@ -0,0 +1,15 @@
+export const theme = {
+  color: {
+    primary: '#D7A63C',
+    seconndary: '#000000',
+    darkBlue: '#1A205D',
+    buttonBg: '#E5E5E5',
+  },
+  fontFamily: {
+    bold: 'Syne-Bold',
+    extraBold: 'Syne-ExtraBold',
+    medium: 'Syne-Medium',
+    regular: 'Syne-Regular',
+    semiBBold: 'Syne-SemiBold',
+  },
+};
