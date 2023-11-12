d6886672090aec56cb50c5d350e2b77a9bedf605
AbdullahJaspal <abdullahafzal4444@gmail.com>
2023-11-12 16:52:29 +0500

    profile screen designed & finished

--- src/screens/profile/profile.js
+++ src/screens/profile/profile.js
@@ -1,5 +1,12 @@
 import React from 'react';
-import {Dimensions, Image, ImageBackground, Text, View} from 'react-native';
+import {
+  Dimensions,
+  Image,
+  ImageBackground,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
 import {theme} from '../../theme/theme';
 const {width, height} = Dimensions.get('screen');
 const Profile = () => {
@@ -7,140 +14,107 @@ const Profile = () => {
     <View style={{flex: 1, backgroundColor: 'white'}}>
       <ImageBackground
         source={require('../../assets/images/profileBg.png')}
-        style={{
-          width: '100%',
-          height: height / 2.555,
-        }}>
-        <View
-          style={{
-            backgroundColor: 'rgba(0,0,0,0.28)',
-            flex: 1,
-            alignItems: 'center',
-            justifyContent: 'center',
-          }}>
+        style={style.topBg}>
+        <View style={style.overLay}>
           <Image
             source={require('../../assets/images/profile.png')}
-            style={{
-              width: 120,
-              height: 120,
-              borderRadius: 100,
-              borderWidth: 2,
-              borderColor: 'white',
-              marginTop: 10,
-            }}
+            style={style.profileImage}
           />
-          <Text style={{color: 'white', fontFamily: theme.fontFamily.bold}}>
-            Kelly Hudson
-          </Text>
-          <Text style={{color: 'white', fontFamily: theme.fontFamily.medium}}>
-            KellyHudson@gmail.com
-          </Text>
+          <Text style={style.name}>Kelly Hudson</Text>
+          <Text style={style.email}>KellyHudson@gmail.com</Text>
         </View>
       </ImageBackground>
-      <View
-        style={{
-          width: '90%',
-          alignSelf: 'center',
-          height: 70,
-          shadowColor: '#000',
-          shadowOffset: {width: 0, height: 2},
-          shadowOpacity: 0.5,
-          shadowRadius: 2,
-          elevation: 2,
-          backgroundColor: 'white',
-          marginTop: 30,
-          borderRadius: 10,
-          flexDirection: 'row',
-          paddingHorizontal: 10,
-          alignItems: 'center',
-          justifyContent: 'space-between',
-        }}>
-        <View
-          style={{
-            height: 45,
-            width: 45,
-            backgroundColor: 'rgba(215,166,60,0.28)',
-            alignItems: 'center',
-            justifyContent: 'center',
-            borderRadius: 10,
-          }}>
+      <View style={style.tabWrap}>
+        <View style={style.iconWrap}>
           <Image
             source={require('../../assets/icons/edit.png')}
-            style={{
-              width: 20,
-              height: 20,
-              resizeMode: 'contain',
-              tintColor: theme.color.primary,
-            }}
+            style={style.icon}
           />
         </View>
 
-        <Text style={{fontFamily: theme.fontFamily.bold, width: '70%'}}>
-          Edit profile
-        </Text>
+        <Text style={style.tabTitle}>Edit profile</Text>
         <Image
           source={require('../../assets/icons/right.png')}
-          style={{
-            width: 20,
-            height: 20,
-            resizeMode: 'contain',
-            tintColor: theme.color.primary,
-          }}
+          style={style.rightIcon}
         />
       </View>
-      <View
-        style={{
-          width: '90%',
-          alignSelf: 'center',
-          height: 70,
-          shadowColor: '#000',
-          shadowOffset: {width: 0, height: 2},
-          shadowOpacity: 0.5,
-          shadowRadius: 2,
-          elevation: 2,
-          backgroundColor: 'white',
-          marginTop: 30,
-          borderRadius: 10,
-          flexDirection: 'row',
-          paddingHorizontal: 10,
-          alignItems: 'center',
-          justifyContent: 'space-between',
-        }}>
-        <View
-          style={{
-            height: 45,
-            width: 45,
-            backgroundColor: 'rgba(215,166,60,0.28)',
-            alignItems: 'center',
-            justifyContent: 'center',
-            borderRadius: 10,
-          }}>
+      <View style={style.tabWrap}>
+        <View style={style.iconWrap}>
           <Image
             source={require('../../assets/icons/recipe.png')}
-            style={{
-              width: 20,
-              height: 20,
-              resizeMode: 'contain',
-              tintColor: theme.color.primary,
-            }}
+            style={style.icon}
           />
         </View>
 
-        <Text style={{fontFamily: theme.fontFamily.bold, width: '70%'}}>
-          My Recipe
-        </Text>
+        <Text style={style.tabTitle}>My Recipe</Text>
         <Image
           source={require('../../assets/icons/right.png')}
-          style={{
-            width: 20,
-            height: 20,
-            resizeMode: 'contain',
-            tintColor: theme.color.primary,
-          }}
+          style={style.rightIcon}
         />
       </View>
     </View>
   );
 };
+
+const style = StyleSheet.create({
+  topBg: {
+    width: '100%',
+    height: height / 2.555,
+  },
+  overLay: {
+    backgroundColor: 'rgba(0,0,0,0.28)',
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  profileImage: {
+    width: 120,
+    height: 120,
+    borderRadius: 100,
+    borderWidth: 2,
+    borderColor: 'white',
+    marginTop: 10,
+  },
+  name: {color: 'white', fontFamily: theme.fontFamily.bold},
+  email: {color: 'white', fontFamily: theme.fontFamily.medium},
+  tabWrap: {
+    width: '90%',
+    alignSelf: 'center',
+    height: 70,
+    shadowColor: '#000',
+    shadowOffset: {width: 0, height: 2},
+    shadowOpacity: 0.5,
+    shadowRadius: 2,
+    elevation: 2,
+    backgroundColor: 'white',
+    marginTop: 30,
+    borderRadius: 10,
+    flexDirection: 'row',
+    paddingHorizontal: 10,
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  iconWrap: {
+    height: 45,
+    width: 45,
+    backgroundColor: 'rgba(215,166,60,0.28)',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 10,
+  },
+  icon: {
+    width: 20,
+    height: 20,
+    resizeMode: 'contain',
+    tintColor: theme.color.primary,
+  },
+  tabTitle: {fontFamily: theme.fontFamily.bold, width: '70%'},
+  rightIcon: {
+    width: 20,
+    height: 20,
+    resizeMode: 'contain',
+    tintColor: theme.color.primary,
+  },
+});
 
 export default Profile;
